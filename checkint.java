82a93ea2b34a145229dfc06215a5ea3301005a02
AmanRoy <72335987+amanroy2001@users.noreply.github.com>
2021-06-19 18:09:42 +0530

    Add files via upload

---
+++ checkint.java
@@ -0,0 +1,13 @@
+package codewithaman;
+import java.util.Scanner;
+public class checkint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		System.out.println("please enter any number");
+System.out.println(sc.hasNextInt());
+
+	}
+
+}
